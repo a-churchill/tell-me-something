39a1342018466c3dcb64122506a1c27b9cb2e4e2
Andrew Churchill <chu.andrew.8@outlook.com>
2018-08-01 15:50:06 +0300

    Upload supplementary files

---
+++ website/request_handler2.py
@@ -0,0 +1,8 @@
+def request_handler(request):
+	with open('__HOME__/finalProject/RH2.html', 'r') as myfile:
+		return myfile.read()
+		
+		
+
+		# test comment
+		# test comment 2. Let's get it!
---
+++ website/request_handler3.py
@@ -0,0 +1,5 @@
+def request_handler(request):
+	with open('__HOME__/finalProject/RH3.html', 'r') as myfile:
+		return myfile.read()
+
+		# test comment
